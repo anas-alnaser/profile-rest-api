7201757857d22099af1401872945ef82d47d2793
anas-alnaser <anasalnaser24@gmail.com>
2025-10-27 02:31:47 +0300

    Added login page use tokens the extention

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,6 +7,8 @@ from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 class HelloApiView(APIView):
@@ -103,3 +105,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
         'name',
         'email',
     )
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authinticaino tokens"""
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
